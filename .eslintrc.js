e430a052ef83b271b457f71584067e866ac8b4d0
fikibrahim <taufikibrahim21@gmail.com>
2021-05-25 20:39:52 +0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "airbnb-base"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-console": "off"
+    }
+};
---
+++ src/routes.js
@@ -0,0 +1,30 @@
+const { addNotesHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler } = require("./handler");
+
+const routes = [{
+        method: 'POST',
+        path: '/notes',
+        handler: addNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        //  handler: editNoteByIdHandler,
+    },
+];
+
+module.exports = routes;
